431c51c4646d92fa6344749bd7ba006a0e1302db
bnwlkr <bnwlkr@icloud.com>
2020-08-23 22:30:50 -0700

    Finished downloading and importing pdf statements from RBC. Next thing to do is make the Transaction objects hashable so that data overlap is OK.

--- pdf_processor.py
+++ pdf_processor.py
@@ -11,7 +11,7 @@ VISA_ACCOUNT_NUM = '451015XXXXXX8313'
 
 VISA_TRANSACTION_REGEX = (r"^(?P<dates>(?:\w{3} \d{2} ){2})"
                           r"(?P<description>.+)\s"
-                          r"(?P<amount>-?\$\d+\.\d{2})")
+                          r"(?P<amount>-?\$[\d,]+\.\d{2})$")
 
 def get_account_type(pdf_file_name):
     if pdf_file_name.startswith(VISA_ACCOUNT_NUM):
@@ -24,6 +24,7 @@ def get_start_year(pdf_file_name):
 def get_transactions(data_directory):
     result = []
     for pdf_path in Path(data_directory).rglob('*.pdf'):
+        print(pdf_path.name)
         with pdfplumber.open(pdf_path) as pdf:
             year = get_start_year(pdf_path.name)
             last_month = None
@@ -34,13 +35,14 @@ def get_transactions(data_directory):
                 match_dict = match.groupdict()
                 account_type = get_account_type(pdf_path.name)
                 date = ' '.join(match_dict['dates'].split(' ')[0:2])
-                date = datetime.strptime(date, '%b %d')
+                month = datetime.strptime(date.split(' ')[0], '%b').month
                 if last_month is not None:
-                    if date.month < last_month:
+                    if month < last_month:
                         year += 1
-                last_month = date.month
-                date = date.replace(year=year, hour=12)
-                amount = float(match_dict['amount'].replace('$', ''))
+                last_month = month
+                date = datetime.strptime(date + ' ' + str(year), '%b %d %Y')
+                date = date.replace(hour=12)
+                amount = float(match_dict['amount'].replace('$', '').replace(',', ''))
                 result.append(Transaction(account_type,
                                           date,
                                           match_dict['description'],
